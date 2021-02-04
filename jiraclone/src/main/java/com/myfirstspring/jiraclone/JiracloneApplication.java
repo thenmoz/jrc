d88a91f8131f73e92544420de4e253a9adeb7f16
Chanut Wongtripipat <chanutwongtripipat@Chanuts-MacBook-Air.local>
2021-02-05 00:07:31 +0700

    first commit

---
+++ jiraclone/src/main/java/com/myfirstspring/jiraclone/JiracloneApplication.java
@@ -0,0 +1,13 @@
+package com.myfirstspring.jiraclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiracloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiracloneApplication.class, args);
+	}
+
+}
---
+++ jiraclone/src/test/java/com/myfirstspring/jiraclone/JiracloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfirstspring.jiraclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiracloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
